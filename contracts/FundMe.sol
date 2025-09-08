4ee3d02c9891d0663692410974c0002c4131579d
YastecHub <yasiroyebo@gmail.com>
2025-09-08 18:58:00 +0100

    feat:add function for funding and withdrawal

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -1,41 +1,57 @@
 // SPDX-License-Identifier: MIT
+
 pragma solidity ^0.8.18;
 
-import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
-
+import {PriceConverter} from "./PriceConverter.sol";
 //ToDo:
 //Get Funds from Users
 //Withdraw the funds
 //Set a minimum value in USD
 contract FundMe{
+   using PriceConverter for uint256;
 
    uint256 public minimumUsd = 5e18;
+
    address[] public funders;
    mapping(address funder => uint256 amountFunded) public addressToAmountFunded;
+
+   address public owner;
+
+   constructor() {
+    owner = msg.sender;
+   }
 
     function fund() public payable {
         //Allow Users to send $
         //Have a minimum $ to sent
-        require(getConversionRate(msg.value) >= minimumUsd, "did send enough ETH");
+        require(msg.value.getConversionRate() >= minimumUsd, "did send enough ETH");
         funders.push(msg.sender);
-        addressToAmountFunded[msg.sender] = addressToAmountFunded[msg.sender] + msg.value;
+        addressToAmountFunded[msg.sender] += msg.value;
     }
 
-    function getPrice() public view returns(uint256){
-        //Address
-        //ABI
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(0xdC08BeB25e27168F34Db5AE04387e9F81E207356);
-        (,int256 price,,,) = priceFeed.latestRoundData();
-        return uint256(price * 1e10);
+    function withdraw() public onlyOwner{
+        for (uint256 funderIndex = 0; funderIndex < funders.length ; funderIndex++){
+            address funder = funders[funderIndex];
+            addressToAmountFunded[funder] = 0;
+        }
+        //reset the array
+        funders = new address[](0);
+
+        //actually withdraw the funds(3-methods)
+        //Transfer
+        // payable(msg.sender).transfer(address(this).balance);
+
+        // //send
+        // bool sendSuccess = payable(msg.sender).send(address(this).balance);
+        // require(sendSuccess, "Send Failed");
+
+        //Call
+        (bool callSuccess, ) = payable(msg.sender).call{value: address(this).balance}("");
+        require(callSuccess, "Call Failed");
     }
 
-    function getConversionRate(uint256 ethAmount) public view returns(uint256){
-        uint256 ethPrice = getPrice();
-        uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1e18;
-        return ethAmountInUsd;
-    }
-
-    function getVersion() public view returns(uint256){
-        return AggregatorV3Interface(0xdC08BeB25e27168F34Db5AE04387e9F81E207356).version();
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Must be the owner , BARAU NIEE!!");
+        _;
     }
 }
